33404b2013f4aaef11ce20827a2db1b83786dc87
Krüpl Zsolt <zsolt@integrity.hu>
2026-07-24 11:09:35 +0200

    rootless docker install script

--- run_in_docker/rootless-docker-install-debian.sh
+++ run_in_docker/rootless-docker-install-debian.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-ROOT_SCRIPT="$(dirname "$0")/rootless-docker-install-as-root-submodule.sh"
+ROOT_SCRIPT="$(pwd)/rootless-docker-install-as-root-submodule.sh"
 
 if command -v sudo >/dev/null 2>&1; then
     echo "User password needed."
-    sudo "bash -c $ROOT_SCRIPT" "$USER"
+    sudo "$ROOT_SCRIPT" "$USER"
 else
     echo "Root password needed."
-    su - -c "bash -c $ROOT_SCRIPT $USER"
+    su - -c "$ROOT_SCRIPT $USER"
 fi
 
-/usr/share/docker.io/contrib/dockerd-rootless-setuptool.sh install --force
+PATH=$PATH:/usr/share/docker.io/contrib/ dockerd-rootless-setuptool.sh install --force
