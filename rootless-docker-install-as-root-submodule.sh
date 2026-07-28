1f0c32f11abd66ddfdeb2e4d8087c44e6e674fd5
Krüpl Zsolt <zsolt@integrity.hu>
2026-07-28 10:29:15 +0200

    rootless docker install script

--- rootless-docker-install-as-root-submodule.sh
+++ rootless-docker-install-as-root-submodule.sh
@@ -5,7 +5,7 @@ USER_NAME="$1"
 apt update
 apt install -y docker.io docker-compose docker-buildx rootlesskit uidmap slirp4netns dbus-user-session
 
-systemctl disable --now docker.service docker.socket
+systemctl disable --now docker.service docker.socket containerd.service
 
 grep -q "^${USER_NAME}:" /etc/subuid || echo "${USER_NAME}:100000:65536" >> /etc/subuid
 grep -q "^${USER_NAME}:" /etc/subgid || echo "${USER_NAME}:100000:65536" >> /etc/subgid
